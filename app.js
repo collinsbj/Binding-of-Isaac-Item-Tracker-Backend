c259964aa33c8c486dea96756bc13c49983c3c42
collinsbj <collinsbj@me.com>
2017-12-11 14:43:22 -0700

    update

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/combinedData", (req,res) => {
-  res.send(addItemPool(items, itemPools));
+  res.send("Hello");
 });
 
 function addItemPool(data1, data2) {
